c3ef491a38d7962cf41762b5f412f5e0cf07f4c6
Luis Salcedo Salas <rideluis@gmail.com>
2022-05-17 18:41:45 -0500

    feat: basic functionality to standing pages

--- src/components/ProfileTournamentItem/ProfileTournamentItem.jsx
+++ src/components/ProfileTournamentItem/ProfileTournamentItem.jsx
@@ -9,14 +9,14 @@ export default function ProfileTournamentItem({ tournament, type }) {
       case 'admin':
         return (
           <>
-            <LinkPrimary path="/tournament/admin/:id">Dashboard</LinkPrimary>
-            <LinkPrimary path="/standing/:id">Standings</LinkPrimary>
+            <LinkPrimary path={`/tournament/admin/${tournament?.id}`}>Dashboard</LinkPrimary>
+            <LinkPrimary path={`/standing/${tournament?.id}`}>Standings</LinkPrimary>
           </>
         );
       case 'participant':
-        return (<LinkPrimary path="/standing/:id">Standings</LinkPrimary>);
+        return (<LinkPrimary path={`/standing/${tournament?.id}`}>Standings</LinkPrimary>);
       case 'finished':
-        return (<LinkPrimary path="/standing/:id">Standings</LinkPrimary>);
+        return (<LinkPrimary path={`/standing/${tournament?.id}`}>Standings</LinkPrimary>);
       default:
         return null;
     }
